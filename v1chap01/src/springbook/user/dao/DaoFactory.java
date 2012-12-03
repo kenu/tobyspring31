17144d74bc7c6644abc921427fb70eaa12844bdd
kenu <kenu.heo@gmail.com>
2012-12-04 04:11:59 +0900

    spring 3.1 jar added

--- v1chap01/src/springbook/user/dao/DaoFactory.java
+++ v1chap01/src/springbook/user/dao/DaoFactory.java
@@ -1,8 +1,13 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import springbook.user.domain.UserDao;
 
+@Configuration
 public class DaoFactory {
+	@Bean
 	public UserDao userDao() {
 		ConnectionMaker connectionMaker = new DConnectionMaker();
 		UserDao userDao = new UserDao(connectionMaker);
