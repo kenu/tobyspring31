60a84c3cf272009022730b6813f01a9babb86d41
kenu <kenu.heo@gmail.com>
2012-12-04 04:36:11 +0900

    add DaoFactoryTest

---
+++ v1chap01/src/springbook/user/dao/DaoFactoryTest.java
@@ -0,0 +1,30 @@
+package springbook.user.dao;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import springbook.user.domain.UserDao;
+
+public class DaoFactoryTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DaoFactory factory = new DaoFactory();
+		UserDao dao1 = factory.userDao();
+		UserDao dao2 = factory.userDao();
+
+		System.out.println(dao1);
+		System.out.println(dao2);
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(
+				DaoFactory.class);
+		
+		UserDao dao3 = context.getBean("userDao", UserDao.class);
+		UserDao dao4 = context.getBean("userDao", UserDao.class);
+		System.out.println(dao3);
+		System.out.println(dao4);
+	}
+
+}
