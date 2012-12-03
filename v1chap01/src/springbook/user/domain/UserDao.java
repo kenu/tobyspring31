32b6ddb7d8955e18750e35cd22c42ff3f89fbb3d
kenu <kenu.heo@gmail.com>
2012-12-04 02:32:48 +0900

    add SimpleConnectionMaker

--- v1chap01/src/springbook/user/domain/UserDao.java
+++ v1chap01/src/springbook/user/domain/UserDao.java
@@ -7,8 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
+	private SimpleConnectionMaker simpleConnectionMaker;
+	
+	public UserDao() {
+		simpleConnectionMaker = new SimpleConnectionMaker();
+	}
+	
     public void add(User user) throws ClassNotFoundException, SQLException {
-    	Connection c = getConnection();
+    	Connection c = simpleConnectionMaker.makeNewConnection();
     	
     	PreparedStatement ps = c.prepareStatement(
     			"insert into users(id, name, password) values(?,?,?)");
@@ -23,7 +29,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-    	Connection c = getConnection();
+    	Connection c = simpleConnectionMaker.makeNewConnection();
     	
     	PreparedStatement ps = c.prepareStatement(
     			"select * from users where id = ?");
@@ -42,14 +48,6 @@ public class UserDao {
     	
 		return user;
     }
-    
-    private Connection getConnection() throws ClassNotFoundException,
-    SQLException {
-    	Class.forName("com.mysql.jdbc.Driver");
-    	Connection c = DriverManager.getConnection(
-    			"jdbc:mysql://localhost/springbook?useUnicode=true&characterEncoding=UTF8&jdbcCompliantTruncation=false&useOldUTF8Behavior=true", "spring", "book");
-    	return c;
-    }
-    
+        
 }
 
