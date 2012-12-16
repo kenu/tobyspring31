2d9dce6e1542c6639dcffdec7a2e17ff30cab022
kenu <kenu.heo@gmail.com>
2012-12-17 02:46:30 +0900

    deleteAll(), getCount()

--- v1chap02/src/springbook/user/dao/UserDaoTest.java
+++ v1chap02/src/springbook/user/dao/UserDaoTest.java
@@ -1,21 +1,17 @@
 package springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.domain.User;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 public class UserDaoTest {
-	public static void main(String[] args) {
-		JUnitCore.main("springbook.user.dao.UserDaoTest");
-	}
 
 	/**
 	 * @throws SQLException 
@@ -28,12 +24,17 @@ public class UserDaoTest {
 		
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
 		User user = new User();
 		user.setId("gyumee");
 		user.setName("박성철");
 		user.setPassword("springno1");
 		
 		dao.add(user);
+		assertThat(dao.getCount(), is(1));
+
 				
 		User user2 = dao.get(user.getId());
 		assertThat(user2.getName(), is(user.getName()));
