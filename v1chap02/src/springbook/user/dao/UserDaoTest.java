697c234ba890724278ee34fab0e60dd2a3f2c2b7
kenu <kenu.heo@gmail.com>
2012-12-17 02:09:30 +0900

    JUnitCore.main()

--- v1chap02/src/springbook/user/dao/UserDaoTest.java
+++ v1chap02/src/springbook/user/dao/UserDaoTest.java
@@ -3,12 +3,19 @@ package springbook.user.dao;
 import java.sql.SQLException;
 
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.domain.User;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
+	public static void main(String[] args) {
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
+	}
 
 	/**
 	 * @throws SQLException 
@@ -22,22 +29,15 @@ public class UserDaoTest {
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
+		user.setId("gyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
 		
 		dao.add(user);
-		
-		System.out.println(user.getId() + " 등록 성공");
-		
+				
 		User user2 = dao.get(user.getId());
-		if (!user.getName().equals(user2.getName())) {
-			System.out.println("테스트 실패 (name)");
-		} else if (!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("테스트 실패 (password)");
-		} else {
-			System.out.println("조회 테스트 성공");
-		}
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
 
 	}
 
