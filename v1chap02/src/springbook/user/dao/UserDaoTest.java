4dcad6ec3600f1ec01e90309b36eda1b4d8bec5f
kenu <kenu.heo@gmail.com>
2012-12-17 01:54:06 +0900

    check data

--- v1chap02/src/springbook/user/dao/UserDaoTest.java
+++ v1chap02/src/springbook/user/dao/UserDaoTest.java
@@ -30,11 +30,13 @@ public class UserDaoTest {
 		System.out.println(user.getId() + " 등록 성공");
 		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		
-		System.out.println(user2.getPassword());
-		
-		System.out.println(user2.getId() + " 조회 성공");
+		if (!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		} else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		} else {
+			System.out.println("조회 테스트 성공");
+		}
 
 	}
 
