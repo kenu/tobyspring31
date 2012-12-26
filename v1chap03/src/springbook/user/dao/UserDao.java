144ba8572e252f28796c32cc1b6b7706f1b692f9
kenu <kenu.heo@gmail.com>
2012-12-26 15:48:26 +0900

    try catch rs

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -86,18 +86,38 @@ public class UserDao {
     }
 
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
+    	Connection c = null;
+    	PreparedStatement ps = null;
+    	ResultSet rs = null;
+    	try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select count(*) from users");
+			rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 		
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count;
 	}
 }
 
