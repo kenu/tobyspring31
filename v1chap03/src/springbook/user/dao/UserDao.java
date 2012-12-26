d65f53b9c846f0073a0e5702766adc0e2d762be3
kenu <kenu.heo@gmail.com>
2012-12-26 19:06:18 +0900

    AddStatement

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -19,18 +19,8 @@ public class UserDao {
 	}
 	
     public void add(User user) throws SQLException {
-    	Connection c = dataSource.getConnection();
-    	
-    	PreparedStatement ps = c.prepareStatement(
-    			"insert into users(id, name, password) values(?,?,?)");
-    	ps.setString(1, user.getId());
-    	ps.setString(2, user.getName());
-    	ps.setString(3, user.getPassword());
-    	
-    	ps.executeUpdate();
-    	
-    	ps.close();
-    	c.close();
+    	StatementStrategy st = new AddStatement(user);
+    	jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
