3efc637de44771646dec531c723c88fda58bfdca
kenu <kenu.heo@gmail.com>
2012-12-26 19:17:02 +0900

    inline deleteAll()

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -67,8 +67,7 @@ public class UserDao {
     		@Override
     		public PreparedStatement makePreparedStatement(Connection c)
     				throws SQLException {
-    			PreparedStatement ps = c.prepareStatement("delete from users");
-    			return ps;
+    			return c.prepareStatement("delete from users");
     		}
 		});
     }
