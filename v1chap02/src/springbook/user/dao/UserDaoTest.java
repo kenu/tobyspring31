2c3cdcb21854677621cf6e9842ec785f01516b6b
kenu <kenu.heo@gmail.com>
2012-12-26 13:37:35 +0900

    @ContextConfiguration

--- v1chap02/src/springbook/user/dao/UserDaoTest.java
+++ v1chap02/src/springbook/user/dao/UserDaoTest.java
@@ -7,13 +7,22 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
+	@Autowired
+	private ApplicationContext context;
+	
 	private UserDao dao;
 	private User user1;
 	private User user2;
@@ -21,10 +30,8 @@ public class UserDaoTest {
 
 	@Before
 	public void setUp() {
-		ApplicationContext context = new GenericXmlApplicationContext(
-				"applicationContext.xml");
 
-		this.dao = context.getBean("userDao", UserDao.class);
+		this.dao = this.context.getBean("userDao", UserDao.class);
 		
 		this.user1 = new User("gyumee", "박성철", "springno1");
 		this.user2 = new User("leegw700", "이길원", "springno2");
