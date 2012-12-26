253ce917d4679bf79dff5134562540c9fe46ea4f
kenu <kenu.heo@gmail.com>
2012-12-26 13:31:40 +0900

    fixture

--- v1chap02/src/springbook/user/dao/UserDaoTest.java
+++ v1chap02/src/springbook/user/dao/UserDaoTest.java
@@ -14,14 +14,22 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
-	UserDao dao;
+	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
 
 	@Before
 	public void setUp() {
 		ApplicationContext context = new GenericXmlApplicationContext(
 				"applicationContext.xml");
 
-		dao = context.getBean("userDao", UserDao.class);
+		this.dao = context.getBean("userDao", UserDao.class);
+		
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
+
 	}
 
 	/**
@@ -30,13 +38,9 @@ public class UserDaoTest {
 	 */
 	@Test
 	public void addAndGet() throws SQLException {
-		
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
-
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
 
 		dao.add(user1);
 		dao.add(user2);
@@ -54,10 +58,6 @@ public class UserDaoTest {
 
 	@Test
 	public void count() throws SQLException {
-		
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
-		User user3 = new User("bumjin", "박범진", "springno3");
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -72,7 +72,7 @@ public class UserDaoTest {
 
 	@Test(expected = EmptyResultDataAccessException.class)
 	public void getUserFailure() throws SQLException {
-		
+
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 
