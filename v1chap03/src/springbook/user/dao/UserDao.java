20a1fc68a8008ba4f57f80950969906ed5a4abcc
kenu <kenu.heo@gmail.com>
2012-12-26 18:39:18 +0900

    micro DI

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -60,32 +60,8 @@ public class UserDao {
     }
     
     public void deleteAll() throws SQLException {
-    	Connection c = null;
-    	PreparedStatement ps = null;
-    	try {
-			c = dataSource.getConnection();
-			
-			StatementStrategy strategy = new DeleteAllStatement();
-			ps = strategy.makePreparedStatement(c);
-			
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-    	
+    	StatementStrategy st = new DeleteAllStatement();
+    	jdbcContextWithStatementStrategy(st);
     }
 
 	public int getCount() throws SQLException {
@@ -107,6 +83,34 @@ public class UserDao {
 				} catch (SQLException e) {
 				}
 			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		
+	}
+
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = dataSource.getConnection();
+			
+			ps = stmt.makePreparedStatement(c);
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
 			if (ps != null) {
 				try {
 					ps.close();
