46dfd908d4e33ece2a40ca78e0f593426103e583
kenu <kenu.heo@gmail.com>
2012-12-26 15:05:07 +0900

    TestObjects

--- v1chap02/src/springbook/learningtest/junit/JUnitTest.java
+++ v1chap02/src/springbook/learningtest/junit/JUnitTest.java
@@ -1,29 +1,31 @@
 package springbook.learningtest.junit;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 import static org.junit.Assert.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class JUnitTest {
-	static JUnitTest testObject;
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 
 	@Test
 	public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	@Test
 	public void test2() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	@Test
 	public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 
 }
