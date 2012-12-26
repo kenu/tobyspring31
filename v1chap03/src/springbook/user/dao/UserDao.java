59ac2cc0759d87880d8ffaaa52e575e1ddd2fd25
kenu <kenu.heo@gmail.com>
2012-12-26 15:44:29 +0900

    SQLException

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -60,13 +60,29 @@ public class UserDao {
     }
     
     public void deleteAll() throws SQLException {
-    	Connection c = dataSource.getConnection();
+    	Connection c = null;
+    	PreparedStatement ps = null;
+    	try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("delete from users");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
     	
-    	PreparedStatement ps = c.prepareStatement("delete from users");
-    	ps.executeUpdate();
-    	
-    	ps.close();
-    	c.close();
     }
 
 	public int getCount() throws SQLException {
