452718d8f4aa09df2d8d399871df5b1c79caca9d
kenu <kenu.heo@gmail.com>
2012-12-26 19:09:56 +0900

    inner class

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -19,6 +19,27 @@ public class UserDao {
 	}
 	
     public void add(User user) throws SQLException {
+    	class AddStatement implements StatementStrategy {
+    		User user;
+    		
+    		public AddStatement(User user) {
+    			this.user = user;
+    		}
+
+    		@Override
+    		public PreparedStatement makePreparedStatement(Connection c)
+    				throws SQLException {
+    			PreparedStatement ps = c
+    					.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+    			ps.setString(1, user.getId());
+    			ps.setString(2, user.getName());
+    			ps.setString(3, user.getPassword());
+
+    			return ps;
+    		}
+
+    	}
+
     	StatementStrategy st = new AddStatement(user);
     	jdbcContextWithStatementStrategy(st);
     }
