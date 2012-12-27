0fb20e24cbe4ec7896a2d183ce905bba95d4eb4e
kenu <kenu.heo@gmail.com>
2012-12-27 17:34:21 +0900

    executeSql moved to JdbcContext

--- v1chap03/src/springbook/user/dao/JdbcContext.java
+++ v1chap03/src/springbook/user/dao/JdbcContext.java
@@ -38,6 +38,14 @@ public class JdbcContext {
 				}
 			}
 		}
-		
+	}
+	public void executeSql(final String query) throws SQLException {
+		workWithStatementStrategy(new StatementStrategy() {
+    		@Override
+    		public PreparedStatement makePreparedStatement(Connection c)
+    				throws SQLException {
+				return c.prepareStatement(query);
+    		}
+		});
 	}
 }
