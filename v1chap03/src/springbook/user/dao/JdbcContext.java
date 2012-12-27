65a1cc16e2a2513b5d474157cb4d9ed772b7047a
kenu <kenu.heo@gmail.com>
2012-12-27 17:01:21 +0900

    JdbcContext

---
+++ v1chap03/src/springbook/user/dao/JdbcContext.java
@@ -0,0 +1,43 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class JdbcContext {
+	private DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void workWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = this.dataSource.getConnection();
+			
+			ps = stmt.makePreparedStatement(c);
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		
+	}
+}
--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -7,19 +7,26 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
+	private JdbcContext jdbcContext;
 	
+	@Autowired
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 	
     public void add(final User user) throws SQLException {
-    	jdbcContextWithStatementStrategy(new StatementStrategy() {
+    	this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 
     		@Override
     		public PreparedStatement makePreparedStatement(Connection c)
@@ -63,7 +70,7 @@ public class UserDao {
     }
     
     public void deleteAll() throws SQLException {
-    	jdbcContextWithStatementStrategy(new StatementStrategy() {
+    	this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
     		@Override
     		public PreparedStatement makePreparedStatement(Connection c)
     				throws SQLException {
@@ -91,34 +98,6 @@ public class UserDao {
 				} catch (SQLException e) {
 				}
 			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-		
-	}
-
-	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		try {
-			c = dataSource.getConnection();
-			
-			ps = stmt.makePreparedStatement(c);
-			
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
 			if (ps != null) {
 				try {
 					ps.close();
