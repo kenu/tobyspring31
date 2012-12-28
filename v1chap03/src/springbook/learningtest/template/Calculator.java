73657575903def5bf3818e5f23a9704264ec4697
kenu <kenu.heo@gmail.com>
2012-12-28 10:11:28 +0900

    close resource

--- v1chap03/src/springbook/learningtest/template/Calculator.java
+++ v1chap03/src/springbook/learningtest/template/Calculator.java
@@ -7,14 +7,27 @@ import java.io.IOException;
 public class Calculator {
 
 	public Integer calcSum(String filepath) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(filepath));
-		Integer sum = 0;
-		String line = null;
-		while((line = br.readLine()) != null) {
-			sum += Integer.valueOf(line);
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			Integer sum = 0;
+			String line = null;
+			while((line = br.readLine()) != null) {
+				sum += Integer.valueOf(line);
+			}
+			return sum;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
 		}
-		br.close();
-		return sum;
 	}
 
 }
