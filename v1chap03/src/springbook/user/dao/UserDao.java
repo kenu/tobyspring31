09553fbeebb7897f03c1cc420c0303f05cc10d6e
kenu <kenu.heo@gmail.com>
2012-12-28 13:11:22 +0900

    query() and no more abstract UserDao

--- v1chap03/src/springbook/user/dao/UserDao.java
+++ v1chap03/src/springbook/user/dao/UserDao.java
@@ -1,9 +1,8 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -12,18 +11,15 @@ import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
-public abstract class UserDao {
-	private DataSource dataSource;
+public class UserDao {
 	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate();
 		this.jdbcTemplate.setDataSource(dataSource);
-
-		this.dataSource = dataSource;
 	}
 
-	public void add(final User user) throws SQLException {
+	public void add(final User user) {
 		this.jdbcTemplate.update(
 				"insert into users(id, name, password) values(?,?,?)",
 				user.getId(), user.getName(), user.getPassword());
@@ -45,14 +41,24 @@ public abstract class UserDao {
 				});
 	}
 
-	public void deleteAll() throws SQLException {
+	public void deleteAll() {
 		this.jdbcTemplate.update("delete from users");
 	}
 
-	abstract protected PreparedStatement makeStatement(Connection c)
-			throws SQLException;
-
 	public int getCount() {
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
+	}
+
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", 
+				new RowMapper<User>() {
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		});
 	}
 }
--- v1chap03/src/springbook/user/dao/UserDaoTest.java
+++ v1chap03/src/springbook/user/dao/UserDaoTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,7 +24,7 @@ public class UserDaoTest {
 	private ApplicationContext context;
 	
 	@Autowired
-	private UserDaoDeleteAll dao;
+	private UserDao dao;
 
 	private User user1;
 	private User user2;
@@ -84,6 +85,34 @@ public class UserDaoTest {
 
 		dao.get("unknown_id");
 
+	}
+	@Test
+	public void getAll() {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+		
+		dao.add(user2);
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));
+		checkSameUser(user2, users2.get(1));
+		
+		dao.add(user3);
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user3, users3.get(0));
+		checkSameUser(user1, users3.get(1));
+		checkSameUser(user2, users3.get(2));
+	}
+
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
 	}
 
 }
