bc77edce1f72476637829d30f447ac02d2a45642
kenu <kenu.heo@gmail.com>
2012-12-28 13:40:20 +0900

    chap04 start

---
+++ v1chap04/src/springbook/user/domain/User.java
@@ -0,0 +1,37 @@
+package springbook.user.domain;
+
+public class User {
+    String id;
+    String name;
+    String password;
+    
+	public User(String id, String name, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+	
+	public User() {
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+}
