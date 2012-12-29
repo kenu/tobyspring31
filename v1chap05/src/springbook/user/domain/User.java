7f84835dedbcfd48ef9fcdf44809a045039d09e3
kenu <kenu.heo@gmail.com>
2012-12-29 15:47:20 +0900

    Level

--- v1chap05/src/springbook/user/domain/User.java
+++ v1chap05/src/springbook/user/domain/User.java
@@ -1,6 +1,16 @@
 package springbook.user.domain;
 
 public class User {
+	private static final int BASIC = 1;
+	private static final int SILVER = 2;
+	private static final int GOLD = 3;
+	
+	int level;
+	
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
     String id;
     String name;
     String password;
