8043110894d371aa1bffbeb9f4694b287ca44149
kenu <kenu.heo@gmail.com>
2012-12-29 16:16:07 +0900

    add update() @T f

--- v1chap05/src/springbook/user/dao/UserDaoTest.java
+++ v1chap05/src/springbook/user/dao/UserDaoTest.java
@@ -145,5 +145,21 @@ public class UserDaoTest {
 
 		}
 	}
+	@Test
+	public void update() {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		
+		user1.setName("오민규");
+		user1.setPassword("springno6");
+		user1.setLevel(Level.GOLD);
+		user1.setLogin(1000);
+		user1.setRecommend(999);
+		dao.update(user1);
+		
+		User user1update = dao.get(user1.getId());
+		checkSameUser(user1, user1update);
+	}
 
 }
