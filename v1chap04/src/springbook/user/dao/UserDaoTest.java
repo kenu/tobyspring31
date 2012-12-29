1f78c8fad3bd094bed89f57b48527d27e74d6ffa
kenu <kenu.heo@gmail.com>
2012-12-29 15:15:51 +0900

    DataAccessException

--- v1chap04/src/springbook/user/dao/UserDaoTest.java
+++ v1chap04/src/springbook/user/dao/UserDaoTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -116,6 +117,14 @@ public class UserDaoTest {
 		assertThat(user1.getId(), is(user2.getId()));
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test(expected=DataAccessException.class)
+	public void duplicateKey() {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		dao.add(user1);
 	}
 
 }
