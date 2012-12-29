9cc833931cd4a9e8f23d5b8caad145a80494d078
kenu <kenu.heo@gmail.com>
2012-12-29 17:01:43 +0900

    upgradeLevels()

---
+++ v1chap05/src/springbook/user/service/UserService.java
@@ -0,0 +1,38 @@
+package springbook.user.service;
+
+import java.util.List;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+public class UserService {
+	UserDao userDao;
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public void upgradeLevels() {
+		List<User> users = userDao.getAll();
+		for (User user : users) {
+			Boolean changed = null;
+			if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+				user.setLevel(Level.SILVER);
+				changed = true;
+			} else if (user.getLevel() == Level.SILVER
+					&& user.getRecommend() >= 30) {
+				user.setLevel(Level.GOLD);
+				changed = true;
+			} else if (user.getLevel() == Level.GOLD) {
+				changed = false;
+			} else {
+				changed = false;
+			}
+			
+			if (changed) {
+				userDao.update(user);
+			}
+		}
+	}
+}
