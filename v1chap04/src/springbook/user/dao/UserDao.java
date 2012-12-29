7277680db5bc9302c0bb2686252ff579fd826cea
kenu <kenu.heo@gmail.com>
2012-12-29 15:12:46 +0900

    extract interface for UserDaoJdbc

--- v1chap04/src/springbook/user/dao/UserDao.java
+++ v1chap04/src/springbook/user/dao/UserDao.java
@@ -1,56 +1,19 @@
 package springbook.user.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
-public class UserDao {
-	private RowMapper<User> userMapper = new RowMapper<User>() {
-		public User mapRow(ResultSet rs, int rowNum)
-				throws SQLException {
-			User user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-			return user;
-		}
-		
-	};
-	private JdbcTemplate jdbcTemplate;
+public interface UserDao {
 
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
+	public abstract void add(User user);
 
-	public void add(final User user) {
-		this.jdbcTemplate.update(
-				"insert into users(id, name, password) values(?,?,?)",
-				user.getId(), user.getName(), user.getPassword());
-	}
+	public abstract User get(String id);
 
-	public User get(String id) {
-		return this.jdbcTemplate.queryForObject(
-				"select * from users where id = ?", new Object[] { id },
-				userMapper);
-	}
+	public abstract void deleteAll();
 
-	public void deleteAll() {
-		this.jdbcTemplate.update("delete from users");
-	}
+	public abstract int getCount();
 
-	public int getCount() {
-		return this.jdbcTemplate.queryForInt("select count(*) from users");
-	}
+	public abstract List<User> getAll();
 
-	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", 
-				userMapper);
-	}
 }
