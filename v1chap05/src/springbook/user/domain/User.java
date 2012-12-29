9aee8aa5b2bc15a8c50381d1b1e874105675a322
kenu <kenu.heo@gmail.com>
2012-12-29 15:50:51 +0900

    Enum Level

--- v1chap05/src/springbook/user/domain/User.java
+++ v1chap05/src/springbook/user/domain/User.java
@@ -1,15 +1,6 @@
 package springbook.user.domain;
 
 public class User {
-	private static final int BASIC = 1;
-	private static final int SILVER = 2;
-	private static final int GOLD = 3;
-	
-	int level;
-	
-	public void setLevel(int level) {
-		this.level = level;
-	}
 	
     String id;
     String name;
