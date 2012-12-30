77bb7898ef3c6ea191d87945c1d7264cc08b7239
kenu <kenu.heo@gmail.com>
2012-12-31 00:45:44 +0900

    MockUserDao

--- v1chap06/src/springbook/user/service/UserServiceTest.java
+++ v1chap06/src/springbook/user/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 import static springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -61,26 +62,31 @@ public class UserServiceTest {
 	}
 	
 	@Test
-	@DirtiesContext
 	public void upgradeLevels() throws Exception {
-		userDao.deleteAll();
-		for (User user : users) userDao.add(user);
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
+		
+		MockUserDao mockUserDao = new MockUserDao(this.users);
+		userServiceImpl.setUserDao(mockUserDao);
 		
 		MockMailSender mockMailSender = new MockMailSender();
 		userServiceImpl.setMailSender(mockMailSender);
 		
 		userServiceImpl.upgradeLevels();
 		
-		checkLevelUpgraded(users.get(0), false);
-		checkLevelUpgraded(users.get(1), true);
-		checkLevelUpgraded(users.get(2), false);
-		checkLevelUpgraded(users.get(3), true);
-		checkLevelUpgraded(users.get(4), false);
+		List<User> updated = mockUserDao.getUpdated();
+		assertThat(updated.size(), is(2));
+		checkUserAndLevel(updated.get(0), "joytouch", Level.SILVER);
+		checkUserAndLevel(updated.get(1), "madnite1", Level.GOLD);
 		
 		List<String> requests = mockMailSender.getRequests();
 		assertThat(requests.size(), is(2));
 		assertThat(requests.get(0), is(users.get(1).getEmail()));
 		assertThat(requests.get(1), is(users.get(3).getEmail()));
+	}
+
+	private void checkUserAndLevel(User updated, String expectedId, Level expectedLevel) {
+		assertThat(updated.getId(), is(expectedId));
+		assertThat(updated.getLevel(), is(expectedLevel));
 	}
 
 	private void checkLevelUpgraded(User user, boolean upgraded) {
@@ -127,6 +133,42 @@ public class UserServiceTest {
 	static class TestUserServiceException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 		
+	}
+	
+	static class MockUserDao implements UserDao {
+		private List<User> users;
+		private List<User> updated = new ArrayList<User>();
+		public MockUserDao(List<User> users) {
+			this.users = users;
+		}
+		public List<User> getUpdated() {
+			return this.updated;
+		}
+		
+		public List<User> getAll() {
+			return this.users;
+		}
+		
+		public void update(User user) {
+			updated.add(user);
+		}
+		
+		public void add(User user) {
+			throw new UnsupportedOperationException();
+		}
+
+		public User get(String id) {
+			throw new UnsupportedOperationException();
+		}
+
+		public int getCount() {
+			throw new UnsupportedOperationException();
+		}
+
+		public void deleteAll() {
+			throw new UnsupportedOperationException();
+		}
+
 	}
 	
 	@Test
