458cf774a2d7b7ab837f599f1ca779b8db17984d
kenu <kenu.heo@gmail.com>
2012-12-30 21:13:25 +0900

    Spring JavaMail

--- v1chap05/src/springbook/user/service/UserService.java
+++ v1chap05/src/springbook/user/service/UserService.java
@@ -1,16 +1,9 @@
 package springbook.user.service;
 
 import java.util.List;
-import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -75,26 +68,16 @@ public class UserService {
 	}
 
 	private void sendUpgradeEMail(User user) {
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "mail.ksug.org");
-		Session s = Session.getInstance(props, null);
-
-		MimeMessage message = new MimeMessage(s);
-		try {
-			message.setFrom(new InternetAddress("useradmin@ksug.org"));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
-					user.getEmail()));
-			message.setSubject("Upgrade 안내");
-			message.setText("사용자님의 등급이 " + user.getLevel().name() + 
-					"로 업그레이드되었습니다.");
-			
-			Transport.send(message);
-		} catch (AddressException e) {
-			throw new RuntimeException(e);
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("mail.server.com");
+		
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(user.getEmail());
+		mailMessage.setFrom("useradmin@ksug.org");
+		mailMessage.setSubject("Upgrade 안내");
+		mailMessage.setText("사용자님의 등급이 " + user.getLevel().name());
+		
+		mailSender.send(mailMessage);
 	}
 
 	public void add(User user) {
