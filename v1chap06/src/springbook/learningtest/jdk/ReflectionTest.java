2ac03aed20e64bd86e0356021e00d45f20633d5b
kenu <kenu.heo@gmail.com>
2012-12-31 02:02:07 +0900

    ReflectionTest

---
+++ v1chap06/src/springbook/learningtest/jdk/ReflectionTest.java
@@ -0,0 +1,28 @@
+package springbook.learningtest.jdk;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+public class ReflectionTest {
+
+	@Test
+	public void invokeMethod() throws Exception {
+		String name = "Spring";
+		
+		// length()
+		assertThat(name.length(), is(6));
+		
+		Method lengthMethod = String.class.getMethod("length");
+		assertThat((Integer)lengthMethod.invoke(name), is(6));
+		
+		assertThat(name.charAt(0), is('S'));
+		
+		Method charAtMethod =  String.class.getMethod("charAt", int.class);
+		assertThat((Character)charAtMethod.invoke(name, 0), is('S'));
+	}
+
+}
