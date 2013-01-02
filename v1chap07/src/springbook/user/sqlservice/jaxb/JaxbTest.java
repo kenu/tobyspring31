32a3ba3e89ae39fb8d7d1175f7cf4936ac04602b
kenu <kenu.heo@gmail.com>
2013-01-02 15:20:51 +0900

    jaxb testcase

---
+++ v1chap07/src/springbook/user/sqlservice/jaxb/JaxbTest.java
@@ -0,0 +1,37 @@
+package springbook.user.sqlservice.jaxb;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.junit.Test;
+
+public class JaxbTest {
+
+	@Test
+	public void readSqlmap() throws JAXBException {
+		String contextPath = Sqlmap.class.getPackage().getName();
+		JAXBContext context = JAXBContext.newInstance(contextPath);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+
+		Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(getClass()
+				.getResourceAsStream("sqlmap.xml"));
+
+		List<SqlType> sqlList = sqlmap.getSql();
+
+		assertThat(sqlList.size(), is(3));
+		assertThat(sqlList.get(0).getKey(), is("add"));
+		assertThat(sqlList.get(0).getValue(), is("insert"));
+		assertThat(sqlList.get(1).getKey(), is("get"));
+		assertThat(sqlList.get(1).getValue(), is("select"));
+		assertThat(sqlList.get(2).getKey(), is("delete"));
+		assertThat(sqlList.get(2).getValue(), is("delete"));
+
+	}
+
+}
