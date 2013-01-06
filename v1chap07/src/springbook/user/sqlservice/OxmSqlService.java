2f4f7b0ce478913eb5a9862730a5f5fbcda2c097
kenu <kenu.heo@gmail.com>
2013-01-06 21:50:43 +0900

    classpath: resource

--- v1chap07/src/springbook/user/sqlservice/OxmSqlService.java
+++ v1chap07/src/springbook/user/sqlservice/OxmSqlService.java
@@ -46,7 +46,7 @@ public class OxmSqlService implements SqlService {
 	}
 
 	private class OxmSqlReader implements SqlReader {
-		private Resource sqlmap = new ClassPathResource("sqlmap.xml", UserDao.class);
+		private Resource sqlmap;
 		
 		public void setSqlmap(Resource sqlmap) {
 			this.sqlmap = sqlmap;
