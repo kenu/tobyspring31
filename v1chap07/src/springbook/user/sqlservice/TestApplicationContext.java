e7da8e6c8d3c8138e9f62b4d0c076d5ec926a8ab
kenu <kenu.heo@gmail.com>
2013-01-07 20:50:53 +0900

    dataSource to java config

--- v1chap07/src/springbook/user/sqlservice/TestApplicationContext.java
+++ v1chap07/src/springbook/user/sqlservice/TestApplicationContext.java
@@ -1,10 +1,26 @@
 package springbook.user.sqlservice;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import com.mysql.jdbc.Driver;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
-
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/testdb?useUnicode=true&characterEncoding=UTF8&jdbcCompliantTruncation=false&useOldUTF8Behavior=true");
+		dataSource.setUsername("spring");
+		dataSource.setPassword("book");
+		
+		return dataSource;
+	}
 }
