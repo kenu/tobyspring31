530a287508d8d529761690cfd4b158c83384e4fc
kenu <kenu.heo@gmail.com>
2013-01-07 23:39:13 +0900

    @Autowired sqlService

--- v1chap07/src/springbook/user/sqlservice/TestApplicationContext.java
+++ v1chap07/src/springbook/user/sqlservice/TestApplicationContext.java
@@ -2,18 +2,27 @@ package springbook.user.sqlservice;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import springbook.user.dao.UserDao;
+import springbook.user.dao.UserDaoJdbc;
+import springbook.user.service.DummyMailSender;
 
 import com.mysql.jdbc.Driver;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
+	@Autowired 
+	SqlService sqlService;
+
 	@Bean
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
@@ -31,5 +40,19 @@ public class TestApplicationContext {
 		DataSourceTransactionManager tm = new DataSourceTransactionManager();
 		tm.setDataSource(dataSource());
 		return tm;
+	}
+	
+	@Bean
+	public MailSender mailSender() {
+		MailSender mailSender = new DummyMailSender();
+		return mailSender;
+	}
+	
+	@Bean
+	public UserDao userDao() {
+		UserDaoJdbc userDao = new UserDaoJdbc();
+		userDao.setDataSource(dataSource());
+		userDao.setSqlService(this.sqlService);
+		return userDao;
 	}
 }
