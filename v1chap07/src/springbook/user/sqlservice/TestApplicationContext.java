0ba739eba70973e15df222b8f90d56d5c93dcadd
kenu <kenu.heo@gmail.com>
2013-01-07 20:55:10 +0900

    transactionManager to java config

--- v1chap07/src/springbook/user/sqlservice/TestApplicationContext.java
+++ v1chap07/src/springbook/user/sqlservice/TestApplicationContext.java
@@ -5,7 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.mysql.jdbc.Driver;
 
@@ -22,5 +24,12 @@ public class TestApplicationContext {
 		dataSource.setPassword("book");
 		
 		return dataSource;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
 	}
 }
